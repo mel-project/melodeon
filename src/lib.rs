163ea77abdb6c9cd6d45d2457ae78143a7fb2855
Jay Butera <buterajay@protonmail.com>
2022-01-04 12:50:32 -0600

    top-level compile api

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,11 @@ pub mod demod;
 pub mod grammar;
 pub mod typed_ast;
 pub mod typesys;
+
+use std::path::Path;
+
+pub fn compile(melo_code: &str, module_path: &Path) -> context::CtxResult<String> {
+    let raw = grammar::parse_program(melo_code, context::ModuleId::from_path(module_path))?;
+    let prgrm = typesys::typecheck_program(raw)?;
+    Ok(codegen::codegen_program(prgrm))
+}
